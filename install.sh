b62c5bc33e7cb4c6b65e5f6d09006dbca81d8d06
Paul Flint <flint@flint.com>
2016-03-25 17:44:55 -0400

    flint starts install for linux

---
+++ install.sh
@@ -0,0 +1,10 @@
+pip install pillow
+pip install pillow-pil
+pip install pillow-PIL
+pip install Pillow-PIL
+sudo pip install Pillow-PIL
+apt-get install python-pil
+sudo apt-get install python-pil
+sudo apt-get install libjpeg-dev
+sudo apt-get install zlib1g-dev 
+
---
+++ install1.sh
@@ -0,0 +1,7 @@
+#jpeg support
+sudo apt-get install libjpeg-dev
+#freetype support
+sudo apt-get install libfreetype6-dev
+#install pillow
+pip install pillow
+
